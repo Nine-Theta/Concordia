7f3b3f84422a812cd9c85ec2a6c338b4d5d94f7b
David <davidheetebrij@gmail.com>
2017-05-09 10:17:16 +0200

    Added a car shadow demo

---
+++ Assets/Scripts/CarHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarHandler : MonoBehaviour {
+
+    public Vector3 StartPosition1;
+    public Vector3 StartPosition2;
+
+    public GameObject Car;
+
+    private float _spawnCooldown;
+
+	private void Start () {
+		
+	}
+	
+	private void FixedUpdate () {
+
+        if (_spawnCooldown <= 0)
+        {
+            _spawnCooldown = Random.Range(0.1f, 4.0f);
+            GameObject car = Instantiate(Car);
+            CarMovement carControl = car.GetComponent<CarMovement>();
+
+            carControl.CarSpeed = Random.Range(0.05f, 0.25f);
+
+            if (Random.Range(0, 2) == 0){
+                carControl.StartPos = gameObject.transform.position + StartPosition1;
+                carControl.EndPos = gameObject.transform.position + StartPosition2;
+            }
+            else{
+                carControl.StartPos = gameObject.transform.position + StartPosition2;
+                carControl.EndPos = gameObject.transform.position + StartPosition1;
+            }
+        }
+        else{
+            _spawnCooldown -= Time.deltaTime;
+        }
+        print(_spawnCooldown);
+	}
+}
---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour {
+    
+    public Vector3 StartPos;
+    public Vector3 EndPos;
+    
+    public float CarSpeed = 0.1f;
+
+    private void Start () {
+        gameObject.transform.position = StartPos;
+
+    }
+	
+	private void FixedUpdate () {
+
+        if (EndPos == Vector3.zero) return;
+
+        if ((EndPos - gameObject.transform.position).magnitude <= 0.5f) Destroy(this.gameObject);
+
+        gameObject.transform.position += Vector3.Scale(Vector3.Normalize(EndPos - StartPos), new Vector3(CarSpeed, CarSpeed, CarSpeed));
+
+        //print("Endpos: " + EndPos + " pos: " + gameObject.transform.position + " length: " + (EndPos - gameObject.transform.position).magnitude);
+    }
+}
