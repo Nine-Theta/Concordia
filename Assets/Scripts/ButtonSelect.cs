63edb11b84776fe68158c4526227bbd1af4031b5
David <davidheetebrij@gmail.com>
2017-05-11 13:23:33 +0200

    Added a pause menu

---
+++ Assets/Scripts/ButtonSelect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSelect : MonoBehaviour {
+
+    private bool _selected = false;
+
+    public bool Selected
+    {
+        set { _selected = value; }
+        get { return _selected; }
+    }
+
+    private void CheckSelected()
+    {
+        if (_selected)
+        {
+            gameObject.GetComponent<Image>().color = Color.grey;
+        }
+
+        if (!_selected)
+        {
+            gameObject.GetComponent<Image>().color = Color.white;
+        }
+    }
+
+    private void Update()
+    {
+        CheckSelected();
+    }
+}
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,23 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour {
 
-    private KeyCode _pauseKey = KeyCode.P; //Default KeyCode, should be changed to the correct one by PlayerMovement
+    private KeyCode _pauseKey = KeyCode.P; //Default KeyCode, should be changed to the correct one by PlayerMovement.
+    private KeyCode _interactKeyLP = KeyCode.E; //Default Interaction KeyCode for the Light Player.
+    private KeyCode _interactKeyDP = KeyCode.Backslash; //Default Interaction KeyCode for the Dark Player.
+    private KeyCode _ctrlPauseKey = KeyCode.JoystickButton9; //Options Button on the PS4 controller, won't be changed.
+    private KeyCode _ctrlInteractKey = KeyCode.JoystickButton1; //X Button on the PS4 controller, might be changed, but probably not due to unofficial button mapping standardization;
+
+    private Image _resume;
+    private Image _quit;
 
     private float _pauseCooldown;
 
 	private void OnEnable ()
     {
-        print("gets called: PauseMenu OnEnable()");
+        foreach (Image buttonImage in gameObject.GetComponentsInChildren<Image>())
+        {
+            string[] splitName = buttonImage.gameObject.name.Split(' ');
+            switch (splitName[0])
+            {
+                case "Resume":
+                    _resume = buttonImage;
+                    break;
+                case "Quit":
+                    _quit = buttonImage;
+                    break;
+            }
+        }
+
+        _resume.gameObject.GetComponent<ButtonSelect>().Selected = true;
+
         Time.timeScale = 0;
         _pauseCooldown = Time.realtimeSinceStartup + 0.1f;
     }
 
     private void OnDisable()
     {
-        print("gets called: PauseMenu OnDisable()");
         Time.timeScale = 1;
     }
 
@@ -26,16 +48,64 @@ public class PauseMenu : MonoBehaviour {
         set { _pauseKey = value; }
         get { return _pauseKey; }
     }
+
+    private void ResumeGame()
+    {
+        print("pressed");
+        _resume.gameObject.GetComponent<ButtonSelect>().Selected = false;
+        _quit.gameObject.GetComponent<ButtonSelect>().Selected = false;
+        gameObject.SetActive(false);
+    }
+
+    private void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    private void MenuNavigation()
+    {
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetAxis("CADPY") < 0)
+        {
+            _resume.gameObject.GetComponent<ButtonSelect>().Selected = false;
+            _quit.gameObject.GetComponent<ButtonSelect>().Selected = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetAxis("CADPY") > 0)
+        {
+            _resume.gameObject.GetComponent<ButtonSelect>().Selected = true;
+            _quit.gameObject.GetComponent<ButtonSelect>().Selected = false;
+        }
+    }
+
+    private void OptionSelection()
+    {
+        if (Input.GetKeyDown(_interactKeyLP) || Input.GetKeyDown(_interactKeyDP) || Input.GetKeyDown(_ctrlInteractKey))
+        {
+            if(_resume.gameObject.GetComponent<ButtonSelect>().Selected)
+            {
+                ResumeGame();
+            }
+
+            if(_quit.gameObject.GetComponent<ButtonSelect>().Selected)
+            {
+                QuitGame();
+            }
+        }
+
+        if (Input.GetKeyDown(_pauseKey) || Input.GetKeyDown(_ctrlPauseKey))
+        {
+            ResumeGame();
+        }
+    }
 	
 	private void Update () {
         if (_pauseCooldown > Time.realtimeSinceStartup) return;
 
-        print("gets called: PauseMenu Update()");
-
-        if (Input.GetKeyDown(_pauseKey))
-        {
-            print("gets called: PauseMenu GetKeyDown");
-            gameObject.SetActive(false);
-        }
+        MenuNavigation();
+        OptionSelection();
 	}
 }
