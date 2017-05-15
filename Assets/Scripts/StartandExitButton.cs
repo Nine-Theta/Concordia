72a8f19649e47547970c47982d358397fe2a376e
LAPTOP-9SEJ32OI\Sisi <432081@student.saxion.nl>
2017-05-15 15:16:55 +0200

    Main Menu

---
+++ Assets/Scripts/StartandExitButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartandExitButton : MonoBehaviour {
+
+		public void Play(){
+			Application.LoadLevel(1);
+		}
+
+	public void Exit(){
+		Application.Quit();
+}
+
+	}
