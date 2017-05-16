f03503505cd076f8b78ccd2b02a224d538969903
David <davidheetebrij@gmail.com>
2017-05-16 14:02:55 +0200

    Added basis for background music (still working on that), added doorSounds, And fixed the pause menu for the new buttons

---
+++ Assets/BackgroundSounds.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSounds : MonoBehaviour {
+
+    public AudioClip backgroundMusic;
+    
+	private void Awake () {
+        if (backgroundMusic != null)
+            GetComponent<AudioSource>().PlayOneShot(backgroundMusic);
+
+    }
+	private void FixedUpdate () {
+        print(GetComponent<AudioSource>().isPlaying);
+        //backgroundMusic.
+    }
+}
---
+++ Assets/DoorSounds.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorSounds : MonoBehaviour {
+
+    public AudioClip doorOpenSound;
+    
+    public void PlayDoorOpenSound()
+    {
+        if(doorOpenSound != null)
+            GetComponent<AudioSource>().PlayOneShot(doorOpenSound);
+    }
+}
