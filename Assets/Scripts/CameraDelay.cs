f9300dedeb539cfd66e47aa15b312d93d7838522
CHRISTIAN\Christian <433910@student.saxion.nl>
2017-05-17 15:15:00 +0200

    Quick upload for CameraDelay

---
+++ Assets/Scripts/CameraDelay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDelay : MonoBehaviour {
+    public float delayInSeconds = 1.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        delayInSeconds -= Time.deltaTime;
+        if(delayInSeconds >= 0.0f)
+        {
+            gameObject.GetComponent<Camera>().enabled = false;
+            this.enabled = false;
+        }
+	}
+}
