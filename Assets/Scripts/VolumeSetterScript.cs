9ca0d3c6aca2eaaad4c29ea71040ea7bbdb58ab3
CHRISTIAN\Christian <433910@student.saxion.nl>
2017-05-18 14:46:09 +0200

    Added a just-in-case script to set the volume of anything with an audiosource

---
+++ Assets/Scripts/VolumeSetterScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeSetterScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PauseMenu.UpdateMusicVolume += UpdateVolume;
+	}
+
+    public void UpdateVolume(float newVolume)
+    {
+        GetComponent<AudioSource>().volume = newVolume;
+    }
+}
